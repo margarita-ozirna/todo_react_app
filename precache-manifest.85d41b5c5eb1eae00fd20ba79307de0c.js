5444c103db0920928575b62ea4039e4e391da842
Rita <ritkapuh1@gmail.com>
2019-06-26 11:15:15 +0300

    Updates

---
+++ precache-manifest.85d41b5c5eb1eae00fd20ba79307de0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895b8aa8afcc5a17a980f16e6bada86b",
+    "url": "/todo_react_app/index.html"
+  },
+  {
+    "revision": "666b84e79c26557aa550",
+    "url": "/todo_react_app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todo_react_app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "666b84e79c26557aa550",
+    "url": "/todo_react_app/static/js/main.714f93de.chunk.js"
+  },
+  {
+    "revision": "0583c8e0969b63e13c75",
+    "url": "/todo_react_app/static/js/runtime~main.c26e3788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo_react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
